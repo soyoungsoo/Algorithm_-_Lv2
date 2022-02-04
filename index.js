a8c776282eb9a1bbd5fcf208b628c0f88d126b02
Youngsoo So <0303soso@gmail.com>
2022-02-04 21:28:20 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+function solution(n) {
+    let answer = [];
+    
+    for (let i = 1; i <= n; i++) {
+        answer.push(new Array(i).fill(0));
+    }
+    
+    let [row, col, curNum] = [-1, 0, 1];
+    
+    for (let i = n; i > 0; i -= 3) {
+        for (let j = 0; j < i; j++) {
+            answer[++row][col] = curNum++;
+        }
+        for (let j = 0; j < i - 1; j++) {
+            answer[row][++col] = curNum++;
+        }
+        for (let j = 0; j < i - 2; j++) {
+            answer[--row][--col] = curNum++;
+        }
+    }
+    
+    return answer.flat();
+}
